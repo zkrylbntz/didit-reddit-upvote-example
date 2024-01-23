b53f76fa5510a5420b8faec52fc8cacd03c5f8da
Shaun Church <shaun@shaunchurch.com>
2024-01-23 13:07:56 +0000

    Install pg-adapter

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import PostgresAdapter from "next-auth/adapters/postgres";
+import PostgresAdapter from "@auth/pg-adapter";
 import { Pool } from "pg";
 
 const db = new Pool({
