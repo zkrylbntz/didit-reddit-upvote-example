234c8ec24e4da921dc3f34c2cdc17a9f9aa1ba21
Shaun Church <shaun@shaunchurch.com>
2024-02-14 21:03:11 +0000

    trustHost must be set to true for next auth

--- src/auth.js
+++ src/auth.js
@@ -6,6 +6,7 @@ import { db } from "./db";
 export const { auth, handlers, signOut, signIn } = NextAuth({
   adapter: PostgresAdapter(db),
   providers: [GitHub],
+  trustHost: true,
   callbacks: {
     session: async ({ session, user }) => {
       session.user.id = user.id;
