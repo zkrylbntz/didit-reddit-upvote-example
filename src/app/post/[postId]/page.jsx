5eb67ba5d634dc2908c9a69beaa8f96f6586d14e
Zak Le Bentz <zakerylebentz@gmail.com>
2024-11-06 15:09:35 +0000

    Posts titles to match post title

--- src/app/post/[postId]/page.jsx
+++ src/app/post/[postId]/page.jsx
@@ -3,6 +3,27 @@ import { CommentList } from "@/components/CommentList";
 import { Vote } from "@/components/Vote";
 import { db } from "@/db";
 
+export async function generateMetadata({ params }) {
+  const postId = params.postId;
+
+  const { rows: posts } = await db.query(
+    `SELECT posts.id, posts.title, posts.body, posts.created_at, users.name, 
+    COALESCE(SUM(votes.vote), 0) AS vote_total
+    FROM posts
+    JOIN users ON posts.user_id = users.id
+    LEFT JOIN votes ON votes.post_id = posts.id
+    WHERE posts.id = $1
+    GROUP BY posts.id, users.name
+    LIMIT 1;`,
+    [postId]
+  );
+  const post = posts[0];
+
+  return {
+    title: `${post.title}`,
+  };
+}
+
 export default async function SinglePostPage({ params }) {
   const postId = params.postId;
 
---
+++ src/app/profile/page.jsx
@@ -0,0 +1,10 @@
+import { db } from "@/db";
+
+export default async function ProfilePage() {
+  return (
+    <>
+      <h1>Add something new to your profile page</h1>
+      <div></div>
+    </>
+  );
+}
